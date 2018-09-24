8e46222303f2bc4c460c2cf852477e9d0ed20403
Zach Nusbaum <zach@zdnmail.com>
2018-09-24 13:45:53 -0700

    Basic server works

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+const app = express();
+const port = 3000;
+
+mongoose.connect(config.database);
+
+app.use(cors());
+
+//Middleware for bodyparsing using both json and urlencoding
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+/*express.static is a built in middleware function to serve static files.
+ We are telling express server public folder is the place to look for the static files
+*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send("Invalid page");
+});
+
+app.listen(port, () => {
+  console.log(`Starting the server at port ${port}`);
+});
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  database: 'mongodb://develop:A12345678@ds251819.mlab.com:51819/helprequests'
+};
